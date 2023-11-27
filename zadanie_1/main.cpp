5ba7faa665426c0bb38b712dea9dcd1c3fc6e027
poole03 <112567094+poole03@users.noreply.github.com>
2023-11-27 23:29:31 +0300

    Update main.cpp

--- zadanie_1/main.cpp
+++ zadanie_1/main.cpp
@@ -1,1 +1,24 @@
+#include "modAlphaCipher.h"
 
+void check(const string& Text, const string& key)
+{
+    string cipherText;
+    string decryptedText;
+    modAlphaCipher cipher(key);
+    cipherText = cipher.encrypt(Text);
+    decryptedText = cipher.decrypt(cipherText);
+    cout<<"key="<<key<<endl;
+    cout<<Text<<endl;
+    cout<<cipherText<<endl;
+    cout<<decryptedText<<endl;
+    if (Text == decryptedText)
+        cout<<"Ok\n";
+    else
+        cout<<"Err\n";
+}
+int main(int argc, char **argv)
+{
+    check("БЫСТРАЯКОРИЧНЕВАЯЛИСАПРЫГАЕТЧЕРЕЗЛЕНИВУЮСОБАКУ","Просто");
+    check("БЫСТРАЯКОРИЧНЕВАЯЛИСАПРЫГАЕТЧЕРЕЗЛЕНИВУЮСОБАКУ","ПРОСТО");
+    //return 0;
+}
